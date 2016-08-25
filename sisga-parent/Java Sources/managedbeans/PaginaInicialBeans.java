31849965efe9ddb0094c117d491e41d96ebd81c1
Umiji Massao <smu_1583@hotmail.com>
2016-08-25 19:47:42 -0300

    PaginaInicialBeans.java

---
+++ sisga-parent/Java Sources/managedbeans/PaginaInicialBeans.java
@@ -0,0 +1,23 @@
+package managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean(name="paginaInicial")
+
+public class PaginaInicialBeans {
+	private String nome = "Nome da Empresa";
+	private String telefone = "XXXX-XXXX";
+	private String endereco = "XXX, XXX, XXX";
+	
+	public String getNome(){
+		return nome;
+	}
+	public String getTelefone(){
+		return telefone;
+	}
+	public String getEndereco(){
+		return endereco;
+	}
+}
